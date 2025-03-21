0a2cf593229f9c83c1a11163e61919ae01232a7f
jamieliu2 <shanjliu@gmail.com>
2025-03-21 17:32:29 -0400

    fixed package structure

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup 
+from setuptools import setup, find_packages
 
 setup( 
 	name='magi_msvgd', 
 	version='1.0', 
 	description='Mitotic Stein variational gradient descent for manifold-constrained Gaussian process inference.', 
 	author='Jamie Liu', 
-	packages=['magi_msvgd'], 
+	packages=find_packages(), 
 	install_requires=[ 
 		'numpy',
 		'scipy',
